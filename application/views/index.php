ad0bbdb8115c317b5353ebf00c69aad48d866b51
FoehnRaja <91128892+FoehnRaja@users.noreply.github.com>
2022-06-11 12:06:29 +0800

    timer
    timer test

--- application/views/index.php
+++ application/views/index.php
@@ -9,6 +9,13 @@
 </head>
 
 <body>
+<form action="">
+    <label>Time:</label>
+    <input type="datetime-local" id=StationTime" name="StationName">
+    <input type="submit">
+    </form>
+
+
     ikaw ay nasa
 
 
@@ -17,23 +24,59 @@
         <option value="EDSA">EDSA</option>
         <option value="Libertad">Libertad</option>
     </select>
-
+   
     <label for="cars">Station</label>
-
+  
     <br>
     <br>
 
-    <canvas id="myCanvas" width="400" height="100" style="border:1px solid #d3d3d3;">
-        Your browser does not support the canvas element.
-    </canvas>
+     <div class="box">
+    <label>Next trail will arrive at 
+     <div class="countdown">00 : 00 : 00 : 00</div>
+     </label>
+     </div>
 
-    <script>
-        var canvas = document.getElementById("myCanvas");
-        var ctx = canvas.getContext("2d");
-        ctx.font = "20px Arial";
-        ctx.fillText("Next train will arrive at --Data--", 10, 50);
-    </script>
+     <script>
+        // Setup End Date for Countdown (getTime == Time in Milleseconds)
+let launchDate = new Date(<?php echo $_GET["StationTime"]; ?>).getTime();
 
+// Setup Timer to tick every 1 second
+let timer = setInterval(tick, 1000);
+
+function tick () {
+  // Get current time
+  let now = new Date().getTime();
+  // Get the difference in time to get time left until reaches 0
+  let t = launchDate - now;
+
+  // Check if time is above 0
+  if (t > 0) {
+    // Setup Days, hours, seconds and minutes
+    // Algorithm to calculate days...
+    let days = Math.floor(t / (1000 * 60 * 60 * 24));
+    // prefix any number below 10 with a "0" E.g. 1 = 01
+    if (days < 10) { days = "0" + days; }
+    
+    // Algorithm to calculate hours
+    let hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    if (hours < 10) { hours = "0" + hours; }
+
+    // Algorithm to calculate minutes
+    let mins = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+    if (mins < 10) { mins = "0" + mins; }
+
+    // Algorithm to calc seconds
+    let secs = Math.floor((t % (1000 * 60)) / 1000);
+    if (secs < 10) { secs = "0" + secs; }
+
+    // Create Time String
+    let time = `${days} : ${hours} : ${mins} : ${secs}`;
+
+    // Set time on document
+    document.querySelector('.countdown').innerText = time;
+  }
+}
+     </script>
 </body>
 
 </html>
