bbf61e171027cefcf1aa09d4c4cac3653830a0d7
Punzaman <kurkkeithkylepunzalan@gmail.com>
2022-07-04 17:50:00 +0800

    Adminlogin Connected

--- application/views/admin/adminlogin.php
+++ application/views/admin/adminlogin.php
@@ -1,43 +1,63 @@
-	<section class="Form my-4 mx-5 w-100 mx-auto text-white">
-		<h1 class="text-center">Admin Panel</h1>
-		<h5 class="text-center">Please Log in to Continue</h5>
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-7 mx-auto bg-dark bg-opacity-50 p-5 mt-5">
-					<h3 class="pb-4 text-center">Admin Log in</h3>
 
-					<form method="POST" class="">
-						<div class="form-row">
-							<div class="col-12">
-								<input type="text" name="adminusername" placeholder="Username" class="form-control my-3 p-3">
-							</div>
-						</div>
-						<div class="form-row">
-							<div class="col-12">
-								<input type="password" name="adminpassword" placeholder="Password" class="form-control my-3 p-3">
-							</div>
-						</div>
+	<!DOCTYPE html>
+<html lang="en">
 
-						<div class="form-row">
-							<div class="col-lg-12 d-flex justify-content-around">
-								<input type="submit" value="login" name="login" class="mt-3 mb-4 col-3">
-							</div>
-						</div>
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
 
-					</form>
-				</div>
-			</div>
-		</div>
-	</section>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-	</body>
+    <!--=== FAVICON ===-->
+    <link rel="shortcut icon" href="<?php echo base_url(); ?>css/img/favicon.ico" type="image/x-icon">
 
-	</html>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-	</body>
+    <!--=== CSS ===-->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>css/admin-login-styles.css" />
 
-	</html>
+    <!--=== FONTS ===-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&family=Spline+Sans+Mono:wght@400;500;600;700&family=Spline+Sans:wght@300;400;500;600;700&family=Syne:wght@400;500;600;700;800&display=swap"
+        rel="stylesheet">
 
-	<!--<?= isset($message) ? $message : ""; ?>
+    <title>Admin Log In</title>
+</head>
+
+<body>
+    <div class="grid-container">
+        <div class="left-panel">
+            <div class="upper-content">
+                <h3>(Sabay)</h3>
+                <h1>Keeping trains on schedule</h1>
+            </div>
+
+            <div class="lower-content">
+                <p>Art by Coen Pohl</p>
+            </div>
+        </div>
+
+        <div class="form-container">
+            <div class="logInForm">
+                <form method="POST" class="">
+                    <h3>(Sabay)</h3>
+                    <h2>Admin Log In</h2>
+
+                    <div class="input-field">
+                        <input type="text" name="adminusername" placeholder="Username" />
+                    </div>
+
+                    <div class="input-field">
+                        <input type="password" name="adminpassword" placeholder="Password" />
+                    </div>
+
+                    <div class="submit-field">
+                        <input type="submit" name="login" value="login" />
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
