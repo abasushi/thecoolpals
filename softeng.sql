687a64c416d33e247a14d9ad74f7bfd623a8348b
Punzaman <kurkkeithkylepunzalan@gmail.com>
2022-07-09 17:36:45 +0800

    Update softeng.sql

--- softeng.sql
+++ softeng.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 26, 2022 at 11:27 AM
+-- Generation Time: Jul 09, 2022 at 11:35 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -62,26 +62,26 @@ CREATE TABLE `station` (
 --
 
 INSERT INTO `station` (`stationId`, `stationName`, `stationHours`, `stationMinutes`, `stationSeconds`, `stationHours2`, `stationMinutes2`, `stationSeconds2`) VALUES
-(1, 'BACLARAN', 18, 45, 0, 0, 0, 0),
-(2, 'EDSA', 19, 0, 0, 0, 0, 0),
-(3, 'LIBERTAD', 18, 45, 0, 0, 0, 0),
-(4, 'GIL PUYAT', 18, 45, 0, 0, 0, 0),
-(5, 'VITO CRUZ', 18, 45, 0, 0, 0, 0),
-(6, 'QUIRINO', 18, 45, 0, 0, 0, 0),
-(7, 'PEDRO GIL', 18, 45, 0, 0, 0, 0),
-(8, 'UNITED NATIONS', 18, 45, 0, 0, 0, 0),
-(9, 'CENTRAL', 18, 45, 0, 0, 0, 0),
-(10, 'CARRIEDO', 18, 45, 0, 0, 0, 0),
-(11, 'DOROTEO JOSE', 18, 45, 0, 0, 0, 0),
-(12, 'BAMBANG', 18, 45, 0, 0, 0, 0),
-(13, 'TAYUMAN', 18, 45, 0, 0, 0, 0),
-(14, 'BLUMENTRITT', 18, 45, 0, 0, 0, 0),
-(15, 'ABAD SANTOS', 18, 45, 0, 0, 0, 0),
-(16, 'R.PAPA', 18, 45, 0, 0, 0, 0),
-(17, '5TH AVENUE', 18, 45, 0, 0, 0, 0),
-(18, 'MONUMENTO', 18, 45, 0, 0, 0, 0),
-(19, 'BALINTAWAK', 18, 45, 0, 0, 0, 0),
-(20, 'ROOSEVELT', 0, 0, 0, 5, 0, 0);
+(1, 'BACLARAN', 5, 1, 0, 5, 52, 0),
+(2, 'EDSA', 5, 3, 0, 5, 50, 0),
+(3, 'LIBERTAD', 5, 7, 0, 5, 46, 0),
+(4, 'GIL PUYAT', 5, 9, 0, 5, 44, 0),
+(5, 'VITO CRUZ', 5, 11, 0, 5, 42, 0),
+(6, 'QUIRINO', 5, 13, 0, 5, 40, 0),
+(7, 'PEDRO GIL', 5, 15, 0, 5, 38, 0),
+(8, 'UNITED NATIONS', 5, 17, 0, 5, 36, 0),
+(9, 'CENTRAL', 5, 20, 0, 5, 33, 0),
+(10, 'CARRIEDO', 5, 23, 0, 5, 30, 0),
+(11, 'DOROTEO JOSE', 5, 25, 0, 5, 28, 0),
+(12, 'BAMBANG', 5, 27, 0, 5, 26, 0),
+(13, 'TAYUMAN', 5, 29, 0, 5, 24, 0),
+(14, 'BLUMENTRITT', 5, 32, 0, 5, 21, 0),
+(15, 'ABAD SANTOS', 5, 35, 0, 5, 18, 0),
+(16, 'R.PAPA', 5, 37, 0, 5, 16, 0),
+(17, '5TH AVENUE', 5, 39, 0, 5, 14, 0),
+(18, 'MONUMENTO', 5, 42, 0, 5, 11, 0),
+(19, 'BALINTAWAK', 5, 46, 0, 5, 7, 0),
+(20, 'ROOSEVELT', 5, 52, 0, 5, 1, 0);
 
 --
 -- Indexes for dumped tables
